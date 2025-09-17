91eeedec0c41e8fd8b085f45e13eccf95ffa39e3
Sephydev <nicolas.bussiere@outlook.fr>
2025-09-17 17:46:13 +0200

    Make header responsive

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -9,28 +9,28 @@ const Navbar = () => {
 
     return (
         <div className="flex">
-            <button onClick={() => setShow(!show)} className="md:hidden">
+            <button onClick={() => setShow(!show)} className="lg:hidden">
                 {
                     show ?
                     <img src={navbarCrossIcon} alt="navbar icon"/> :
                     <img src={navbarBarsIcon} alt="navbar icon"/>
                 }
             </button>
-            <div className={`absolute px-4 bg-white top-18 w-screen h-screen transition-all ${show ? "right-0" : "right-full"}`}>
-                <nav className="border-y border-gray-200 pt-4">
-                    <ul className="font-medium">
-                        <li className="pb-4"><a href="#">About</a></li>
-                        <li className="pb-4"><a href="#">Work</a></li>
-                        <li className="pb-4"><a href="#">Testimonials</a></li>
+            <div className={`absolute px-4 bg-white top-18 w-screen h-screen transition-all ${show ? "right-0" : "right-full"} lg:static lg:w-auto lg:h-auto lg:flex lg:flex-row lg:items-center`}>
+                <nav className="border-y border-gray-200 pt-4 lg:border-none">
+                    <ul className="font-medium lg:flex lg:mr-12">
+                        <li className="pb-4 lg:mr-6"><a href="#">About</a></li>
+                        <li className="pb-4 lg:mr-6"><a href="#">Work</a></li>
+                        <li className="pb-4 lg:mr-6"><a href="#">Testimonials</a></li>
                         <li className="pb-4"><a href="#">Contact</a></li>
                     </ul>
                 </nav>
-                <div className="flex flex-col p-4">
-                    <button className="flex justify-between mb-4 items-center">
-                        <span>Switch Theme</span>
-                        <img src={sunIcon} alt="theme icon" className="inline" />
+                <div className="flex flex-col p-4 lg:flex-row">
+                    <button className="flex justify-between mb-4 lg:m-0 lg:mr-4 items-center">
+                        <span className="lg:hidden">Switch Theme</span>
+                        <img src={sunIcon} alt="theme icon" />
                     </button>
-                    <button className="bg-black text-white p-1 rounded-xl">Download CV</button>
+                    <button className="bg-black m-auto text-white p-1 w-2xs lg:w-auto lg:px-4 rounded-xl">Download CV</button>
                 </div>
             </div>
         </div>
