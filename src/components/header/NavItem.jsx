96d073668f70b4bdd022604656f2232964803e34
Sephydev <nicolas.bussiere@outlook.fr>
2025-10-01 21:17:31 +0200

    Refactor navbar

---
+++ src/components/header/NavItem.jsx
@@ -0,0 +1,7 @@
+const NavItem = ({ item, setShow, show, darkMode }) => {
+    return (
+        <li className="pb-4 lg:mr-6"><a href={`#${item}`} onClick={() => setShow(!show)} className={`${darkMode ? "text-white" : "text-black"} text-black hover:text-blue-500 transition-all duration-300`}>{item}</a></li>
+    )
+}
+
+export default NavItem
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import NavItem from './NavItem'
+
 import navbarBarsIcon from '../../assets/icons/navbar-bars-icon.svg'
 import navbarCrossIcon from '../../assets/icons/navbar-cross-icon.svg'
 import sunIcon from '../../assets/icons/sun-icon.svg'
@@ -7,6 +9,8 @@ import moonIcon from '../../assets/icons/moon-icon.svg'
 
 const Navbar = ({darkMode, setDarkMode}) => {
     const [show, setShow] = useState(false)
+
+    const navItems = ["About", "Skills", "Contact"]
 
     return (
         <div className="flex">
@@ -20,9 +24,9 @@ const Navbar = ({darkMode, setDarkMode}) => {
             <div className={`${darkMode ? "bg-black text-white" : "bg-white"} absolute pl-4 top-17 w-screen h-screen transition-all ${show ? "right-0" : "right-[100rem]"} lg:static lg:w-auto lg:h-auto lg:flex lg:flex-row lg:items-center`}>
                 <nav className={`border-y mr-5 ${darkMode ? "border-gray-700" : "border-gray-200"} pt-4 lg:border-none`}>
                     <ul className="font-medium lg:px-0 lg:flex lg:mr-12">
-                        <li className="pb-4 lg:mr-6"><a href="#about" onClick={() => setShow(!show)} className={`${darkMode ? "text-white" : "text-black"} text-black hover:text-blue-500 transition-all duration-300`}>About</a></li>
-                        <li className="pb-4 lg:mr-6"><a href="#skills" onClick={() => setShow(!show)} className={`${darkMode ? "text-white" : "text-black"} text-black hover:text-blue-500 transition-all duration-300`}>Skills</a></li>
-                        <li className="pb-4 lg:mr-6"><a href="#contact" onClick={() => setShow(!show)} className={`${darkMode ? "text-white" : "text-black"} text-black hover:text-blue-500 transition-all duration-300`}>Contact</a></li>
+                        {navItems.map(i => (
+                            <NavItem key={i} item={i} setShow={setShow} show={show} darkMode={darkMode} />
+                        ))}
                     </ul>
                 </nav>
                 <div className={`flex flex-col py-4 mr-5 lg:flex-row lg:border-l ${darkMode ? "border-gray-800" : "border-gray-200"}`}>
