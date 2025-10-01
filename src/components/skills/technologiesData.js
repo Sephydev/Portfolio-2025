463139ee8a4c162bdaf5827b1e43efccb2f80757
Sephydev <nicolas.bussiere@outlook.fr>
2025-10-01 21:27:06 +0200

    Move technologiesIcons into icons

--- src/components/skills/technologiesData.js
+++ src/components/skills/technologiesData.js
@@ -1,15 +1,15 @@
-import HTML5 from "../../assets/img/technologiesIcon/HTML5.svg"
-import CSS3 from "../../assets/img/technologiesIcon/CSS3.svg"
-import JavaScript from "../../assets/img/technologiesIcon/JavaScript.svg"
-import React from "../../assets/img/technologiesIcon/React.svg"
-import MongoDB from "../../assets/img/technologiesIcon/MongoDB.svg"
-import NodeJS from "../../assets/img/technologiesIcon/Node.js.svg"
-import Express from "../../assets/img/technologiesIcon/Express.svg"
-import TailwindCSS from "../../assets/img/technologiesIcon/Tailwind CSS.svg"
-import Playwright from "../../assets/img/technologiesIcon/Playwrite.svg"
-import Bash from "../../assets/img/technologiesIcon/Bash.svg"
-import Git from "../../assets/img/technologiesIcon/Git.svg"
-import Redux from '../../assets/img/technologiesIcon/Redux.svg'
+import HTML5 from "../../assets/icons/technologiesIcon/HTML5.svg"
+import CSS3 from "../../assets/icons/technologiesIcon/CSS3.svg"
+import JavaScript from "../../assets/icons/technologiesIcon/JavaScript.svg"
+import React from "../../assets/icons/technologiesIcon/React.svg"
+import MongoDB from "../../assets/icons/technologiesIcon/MongoDB.svg"
+import NodeJS from "../../assets/icons/technologiesIcon/Node.js.svg"
+import Express from "../../assets/icons/technologiesIcon/Express.svg"
+import TailwindCSS from "../../assets/icons/technologiesIcon/Tailwind CSS.svg"
+import Playwright from "../../assets/icons/technologiesIcon/Playwrite.svg"
+import Bash from "../../assets/icons/technologiesIcon/Bash.svg"
+import Git from "../../assets/icons/technologiesIcon/Git.svg"
+import Redux from '../../assets/icons/technologiesIcon/Redux.svg'
 
 const technologiesData = [
     {
