cfe91d794705aeac15a6cf7e4a1acb4981eb75ab
Sephydev <nicolas.bussiere@outlook.fr>
2025-10-01 21:35:46 +0200

    Make copy of contact info when clicking on them

--- src/components/contact/ContactInfo.jsx
+++ src/components/contact/ContactInfo.jsx
@@ -2,7 +2,7 @@ import copyIcon from "../../assets/icons/copy-icon.svg"
 
 const ContactInfo = ({ icon, info, type}) => {
     return (
-        <button className="flex mb-4 hover:text-blue-500 transition-all duration-300">
+        <button onClick={() => navigator.clipboard.writeText(info)}className="flex mb-4 hover:text-blue-500 transition-all duration-300">
             <img src={icon} alt={type} />
             <span className="text-lg lg:text-4xl font-semibold mx-4">{info}</span>
             <img src={copyIcon} alt="copy" />
