a1be26bc3117db3b32cde7931ecfae9d2ec9048b
Sephydev <nicolas.bussiere@outlook.fr>
2025-10-01 21:23:24 +0200

    Fix navItems link

--- src/components/header/NavItem.jsx
+++ src/components/header/NavItem.jsx
@@ -1,6 +1,6 @@
 const NavItem = ({ item, setShow, show, darkMode }) => {
     return (
-        <li className="pb-4 lg:mr-6"><a href={`#${item}`} onClick={() => setShow(!show)} className={`${darkMode ? "text-white" : "text-black"} text-black hover:text-blue-500 transition-all duration-300`}>{item}</a></li>
+        <li className="pb-4 lg:mr-6"><a href={`#${item.toLowerCase()}`} onClick={() => setShow(!show)} className={`${darkMode ? "text-white" : "text-black"} text-black hover:text-blue-500 transition-all duration-300`}>{item}</a></li>
     )
 }
 
